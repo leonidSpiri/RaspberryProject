2e6379a6bab781a99c658eed9a93d7437c6dd867
Юрий Спиридонов <2003.leonid2003@gmail.com>
2021-11-05 18:50:46 +0300

    cool shit

--- humidity.py
+++ humidity.py
@@ -2,69 +2,60 @@ import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
 import datetime
+import psycopg2
+from psycopg2 import sql
 
+conn = psycopg2.connect(dbname='d8uq0o35eq55kl', user='zftenjusikiyjk',
+                        password='7369c86979b2cbf92b10879ec08ba1ca99394ea761c0462a4baf24d3a2225685',
+                        host='ec2-176-34-168-83.eu-west-1.compute.amazonaws.com')
+cursor = conn.cursor()
 SENSOR = Adafruit_DHT.AM2302
-PIN = 22
-LED = 23
-SOUND = 25
-LED2 = 27
-
 GPIO.cleanup()
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED2, GPIO.OUT)
-GPIO.output(LED2, GPIO.LOW)
-GPIO.setup(LED, GPIO.OUT)
-GPIO.output(LED, GPIO.LOW)
-GPIO.setup(SOUND, GPIO.OUT)
-GPIO.output(SOUND, GPIO.LOW)
+mainPin = 22
 
-try:
-    while True:
-        humidity, temperature = Adafruit_DHT.read_retry(SENSOR, PIN)
-        f = open("/home/pi/Documents/logTempHum.txt", "a")
-        date = str(datetime.datetime.now())
-        if humidity is not None and temperature is not None:
-            f.write(date + ":    Temp={0:0.1f}C   Humidity={1:0.1f}%\n".format(temperature, humidity))
+
+def convertTemp(value):
+    return str("{0:0.1f}".format(value))
+
+
+def convertHum(value):
+    return str("{0:0.1f}".format(value))
+
+
+def sensorWork(pin: int):
+    previous = 45
+
+    try:
+        while True:
+            humidity, temperature = Adafruit_DHT.read_retry(SENSOR, pin)
+            date = str(datetime.datetime.now())
             print(date + ":     Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-
-            if humidity > 75:
-                while humidity > 75:
-                    GPIO.output(LED2, GPIO.HIGH)
-                    time.sleep(0.3)
-                    GPIO.output(LED2, GPIO.LOW)
-                    time.sleep(0.3)
-                    GPIO.output(LED, GPIO.HIGH)
-                    time.sleep(0.3)
-                    GPIO.output(LED, GPIO.LOW)
-                    humidity, temerature = Adafruit_DHT.read_retry(SENSOR, PIN)
-            if humidity > 62:
-                # f.write(date + ":    Temp={0:0.1f}*C  Humidity={1:0.1f}%\n".format(temperature, humidity))
-                GPIO.output(LED2, GPIO.HIGH)
-                GPIO.output(LED, GPIO.HIGH)
+            if humidity is not None and temperature is not None:
+                with conn.cursor() as cursor:
+                    conn.autocommit = True
+                    values = [
+                        (date, convertTemp(temperature), convertHum(humidity))
+                    ]
+                    insert = sql.SQL('INSERT INTO home (date, temperature, humidity) VALUES {}').format(
+                        sql.SQL(',').join(map(sql.Literal, values))
+                    )
+                    cursor.execute(insert)
+            if abs(humidity - previous) > 5:
+                timeout = 60
             else:
-                if humidity > 45:
-                    GPIO.output(LED2, GPIO.LOW)
-                    GPIO.output(LED, GPIO.HIGH)
+                if abs(humidity - previous) > 2:
+                    timeout = 150
                 else:
-                    GPIO.output(LED, False)
-                    GPIO.output(LED2, False)
-                    pwm = GPIO.PWM(LED, 100)
-                    pwm.start(10)
+                    timeout = 360
+            time.sleep(timeout)
+            previous = humidity
+    except KeyboardInterrupt:
+        GPIO.cleanup()
 
-                while humidity <= 45:
-                    for i in range(0, 101):
-                        pwm.ChangeDutyCycle(i)
-                        time.sleep(0.3)
-                    time.sleep(5)
-                    for i in range(100, -1, -1):
-                        pwm.ChangeDutyCycle(i)
-                        time.sleep(0.3)
-                    time.sleep(3)
-                humidity, temperature = Adafruit_DHT.read_retry(SENSOR, PIN)
-                pwm.stop()
-        else:
-            f.write(date + ":    Failed to retrieve data from humidity sensor\n")
-    f.close()
-    time.sleep(300)
-except KeyboardInterrupt:
-    GPIO.cleanup()
+
+with conn.cursor() as cursor:
+    conn.autocommit = True
+    insert = sql.SQL("DELETE FROM home WHERE date < CURRENT_TIMESTAMP - INTERVAL '2day'")
+    cursor.execute(insert)
+
+sensorWork(mainPin)
